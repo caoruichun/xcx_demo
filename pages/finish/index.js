10a86a7b331881fdb12af7d9ce8a2807ee65664d
caoruichun <360825694@qq.com>
2018-08-30 19:12:43 +0800

    引入iview

--- pages/finish/index.js
+++ pages/finish/index.js
@@ -8,14 +8,31 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    scrollTop: 0,
+    targetTime: 0,
+    targetTime1: 0,
+    myFormat: ['时', '分', '秒'],
+    myFormat1: ['天', '时', '分', '秒'],
+    status: '进行中...',
+    clearTimer: false
   },
-
+  onChange(event) {
+    console.log(event.detail, 'click right menu callback data')
+  },
+  onPageScroll(event) {
+    this.setData({
+      scrollTop: event.scrollTop
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    this.setData({
+      targetTime: new Date().getTime() + 6430000,
+      targetTime1: new Date().getTime() + 86430000,
+      targetTime2: new Date().getTime() + 10000
+    });
   },
 
   /**
@@ -43,9 +60,15 @@ Page({
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+    this.setData({
+      clearTimer: true
+    });
   },
-
+  myLinsterner(e) {
+    this.setData({
+      status: '结束'
+    });
+  },
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
