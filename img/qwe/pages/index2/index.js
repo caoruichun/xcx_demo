2899d89cde4ebe7a2413089d82cfc865d5e2c2d5
魔都叛徒 <caoruichun@yeah.net>
2018-09-19 00:55:38 +0800

    Add files via upload

---
+++ img/qwe/pages/index2/index.js
@@ -0,0 +1,60 @@
+Page({
+  data: {
+
+  },
+  openActionsheet() {
+    wx.showActionSheet({
+      itemList: ['录像', '选取现有的','拍照'],
+      itemColor: '#007aff',
+      success(res) {
+        console.log(res.tapIndex);
+        if (res.tapIndex === 0) {
+          wx.chooseVideo({
+            sourceType: ['camera'],
+            success(res) {
+              console.log(res.tempFilePath);
+            }
+          })
+        } else if (res.tapIndex === 1) {
+          wx.chooseImage({
+            count: 8, // 设置最多三张
+            sizeType: ['original', 'compressed'],
+            sourceType: ['album', 'camera'],
+            success(res) {
+              var tempFilePaths = res.tempFilePaths;
+              // 图片预览
+              wx.previewImage({
+                current: res.tempFilePaths[0],
+                urls: res.tempFilePaths
+              })
+            }
+          })
+        }
+      }
+    })
+  },
+  openActionsheetDe(e) {
+    wx.showActionSheet({
+      itemList: ['删除信息'],
+      itemColor: '#FF3B30',
+      success(res) {
+        wx.showToast({ title: '删除成功！' })
+      }
+    })
+  },
+  openActionsheetSh(e) {
+    wx.showActionSheet({
+      itemList: ['回复', '转发', '打印'],
+      itemColor: '#007aff',
+      success(res) {
+        if (res.tapIndex === 0) {
+          wx.showToast({ title: '回复成功！' });
+        } else if (res.tapIndex === 1) {
+          wx.showToast({ title: '转发成功！' });
+        } else if (res.tapIndex === 2) {
+          wx.showToast({ title: '打印成功！' });
+        }
+      }
+    })
+  }
+})
